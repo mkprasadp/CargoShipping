69a69521ef8bfd4e53e5a541b682c21b477ed368
mprasadp <manikantaprasad04@gmail.com>
2025-06-30 22:14:30 +0530

    cargo

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async(req,res)=>{
     try{
         mongoose.connection.on('connected',()=>{console.log('Database connected')})
-        await mongoose.connect('mongodb+srv://cargo:Admincargo@cluster0.iwyfwdu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('#');
     }catch(error){
         console.log('Database not connected');
     }
